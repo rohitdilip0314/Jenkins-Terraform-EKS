8fcdd71bb91e30019c0673518cad31317ed72d54
rohitdilip0314 <rohitdilip392004@gmail.com>
2026-03-25 02:18:47 -0800

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,12 @@
 locals {
     region = "us-east-1"
     name = "rohit_eks_cluster"
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+}
     vpc_cidr = "10.123.0.0/16"
     azs = ["us-east-1a", "us-east-1b"]
     public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]

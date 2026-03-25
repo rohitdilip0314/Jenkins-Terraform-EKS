ae8fdbcd3dbf64cf278662296bd04cb51ba0df4e
rohitdilip0314 <rohitdilip392004@gmail.com>
2026-03-25 02:14:54 -0800

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,7 +1,8 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
 
-  cluster_name = local.name
+  cluster_name = "my-eks-cluster"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets

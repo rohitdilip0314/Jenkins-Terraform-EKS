2e41e5c3fc08aaa1c3c64b35efab7c38bbc2c59e
rohitdilip0314 <rohitdilip392004@gmail.com>
2026-03-25 02:20:41 -0800

    Update provider.tf for Terraform version and locals
    Updated provider configuration to require Terraform version 1.5 or higher and adjusted local variables for clarity.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,22 +1,29 @@
-locals {
-    region = "us-east-1"
-    name = "rohit_eks_cluster"
-    required_providers {
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
 }
-    vpc_cidr = "10.123.0.0/16"
-    azs = ["us-east-1a", "us-east-1b"]
-    public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
-    private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-    intra_subnets = ["10.123.5.0/24", "10.123.6.0/24"]
-    tags = {
-        example = local.name
-    }
+
+locals {
+  region = "us-east-1"
+  name = "rohit_eks_cluster"
+
+  vpc_cidr = "10.123.0.0/16"
+  azs = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets = ["10.123.5.0/24", "10.123.6.0/24"]
+
+  tags = {
+    example = "rohit_eks_cluster"
+  }
 }
 
 provider "aws" {
-    region = local.region
+  region = local.region
 }

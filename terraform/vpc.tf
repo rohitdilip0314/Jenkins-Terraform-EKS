3cdd359c82414d48d571b45191d71529dc01f2ac
rohitdilip0314 <rohitdilip392004@gmail.com>
2026-03-25 01:42:15 -0800

    Refactor VPC module configuration for clarity

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,22 +1,12 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+    source = "terraform-aws-modules/vpc/aws"
+    version = "~> 4.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
-
-  enable_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
+    name = local.name
+    cidr = local.vpc_cidr
+    azs = local.azs
+    public_subnets = local.public_subnets
+    private_subnets = local.private_subnets
+    intra_subnets = local.intra_subnets
+    enable_nat_gateway = true
 }
